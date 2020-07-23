4c8c7c069c74fd83eba3dc09bfb70a4d71baef83
Iamtanzim <tanzim11@gmail.com>
2020-07-23 15:27:01 +0600

    2-ArrayofObjectslooped

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-import React from 'react';
 
-import './App.css';
-import Fruits from './Fruits';
+  import React, { Component } from 'react';
+import Fruits from './Fruits'
+// import Ninjas from './Ninjas'
 
-function App() {
-  return (
-    <div className="App">
-     App page ssas
-     <Fruits name="Apple" country="Finland"/>
-     <Fruits name="Guava" country="Denmark"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+  fruits: [
+         {name:'Apple',country:'Finland',price:5},   
+         {name:'WaterMelon',country:'Austria',price:5},   
+         {name:'Orange',country:'Venice',price:5}   
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app</h1>
+        <Fruits fruits={this.state.fruits}/>
+      </div>
+    );
+  }
 }
 
+
+
+// class App extends Component {
+//   state = {
+//     ninjas: [
+//       { name: 'Ryu', age: 30, belt: 'black' },
+//       { name: 'Yoshi', age: 20, belt: 'green' },
+//       { name: 'Crystal', age: 25, belt: 'pink' }
+//     ]
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>My first React app</h1>
+//         <Ninjas ninjas={this.state.ninjas}/>
+//       </div>
+//     );
+//   }
+// }
+
 export default App;
+
+
+
--- src/Fruits.js
+++ src/Fruits.js
@@ -1,24 +1,24 @@
- import React, { Component } from 'react'
 
-export default class Fruits extends Component {
+import React, { Component } from 'react'
 
-    constructor(props) {
-        super(props)
-        this.state = {
-             
-        }
-    }
-
-    render() {
-         const {
-             name,
-             country
-         } = this.props;
-        return (
-            <div className="App">
-               <h1>Name-{name}</h1>
-               <h1>Country-{country}</h1>
-            </div>
-        )
-    }
+class Fruits extends Component{
+  render(){
+    const { fruits} = this.props;
+    const fruitsList = fruits.map((fruit,index) => {
+      return (
+        <div className="fruits-list" key={index}>
+          <div>Name: { fruit.name }</div>
+          <div>Age: { fruit.country }</div>
+          <div>Belt: { fruit.price }</div>
+        </div>
+      )
+    });
+    return (
+      <div className="fruit-list">
+        { fruitsList }
+      </div>
+    )
+  }
 }
+
+export default Fruits
