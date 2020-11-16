339f69f29546ccd009fb56759682304e24d47e73
iamtanzim <tanzim67@gmail.com>
2020-11-16 16:51:52 +0600

    3-LifeCycle Hooks with Class Based Components

--- indepthreact/src/App.js
+++ indepthreact/src/App.js
@@ -1,6 +1,6 @@
 
 import React  from 'react';
-
+import Images from  './components/Images';
 
 
 
@@ -8,50 +8,65 @@ import React  from 'react';
 class App extends React.Component{
 
 constructor(props) {
+
+  console.log("App Constructor");
   super(props)
 
-  this.state = {
-     title:'New  Image ',
-     isShowing:false,
-    
-  }
-   this.toggleButton=this.toggleButton.bind(this)
+ this.state={
+   title:'New Image',
+   isShowing:false,
+ }
+
+ 
 }
-//You must use a fat arrow function because in normal function scope of this is notavailable
-//If you want to use you need to bind this
-//  toggleButton=()=>{
-//     this.setState({isShowing:!this.state.isShowing})
-//  } 
-toggleButton(){
-    this.setState({isShowing:!this.state.isShowing})
+
+
+
+
+
+
+toggleButton=()=>{
+  this.setState({isShowing:!this.state.isShowing});
+}
+
+componentDidMount (){
+  console.log("Component did Mount");
+  // this.setState({title:'Title changed'});
+}
+componentDidUpdate(){
+  console.log("Component Updated");
 }
   render() {
-    
+     console.log("App render");
 
  
     //state is react are immutable
     return(
-  
+       
       <section className="flex justify-center items-center">
       
             <div className="w-1/2">
               <div className="my-4 text-center">
                           <p className="text-2xl">{ this.state.title}</p> 
+                   
                           <button 
                           className="bg-blue-800 text-white px-6 py-4 rounded-xl"
-                        onClick={this.toggleButton}
-                          
+                          onClick={this.toggleButton}
+                          alt="Toggle Image"
                           >Toggle Image</button>
-              </div>
-                 
-             
-           {
+                          <div>
+                           
+                          </div>
            
-         this.state.isShowing  ? (
-          <img  className="" src="https://images.unsplash.com/photo-1605036927141-938ec066488f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" class="" alt=""></img>
+              </div>
+                 {
+                this.state.isShowing  ? (
+                  <Images/>
              ):null
-        
-  }
+             }
+                 
+          
+      
                  
              
             
---
+++ indepthreact/src/components/Images.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export default class Images extends Component {
+  constructor(props) {
+      super(props);
+  
+     
+      this.state={
+          interval:null,
+      }
+  }
+  
+   
+    componentDidMount (){
+      
+        
+           this.setState({
+               interval:setInterval(() => {
+               console.log("Hello");
+          }, 1000),
+
+        });
+          
+}
+ componentWillUnmount(){
+        console.log("Component will unmount");
+       clearInterval(this.state.interval);
+    
+    }
+    render() {
+        return (
+            <div>
+                <img  className="" src="https://images.unsplash.com/photo-1605036927141-938ec066488f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt=""></img>
+            </div>
+        )
+    }
+}
