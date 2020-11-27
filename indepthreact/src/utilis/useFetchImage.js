dfb66b930b35938aa7bf42626bc5d411d2459fc9
iamtanzim <tanzim67@gmail.com>
2020-11-27 18:25:20 +0600

    23-Refactoring search and getrandomPage

--- indepthreact/src/utilis/useFetchImage.js
+++ indepthreact/src/utilis/useFetchImage.js
@@ -11,43 +11,57 @@ export default function useFetchImage(page,searchTerm) {
        const [images,setImage] = useState([]);
        const [errors, setErrors] = useState([]);
        const [loading, setLoading] = useState(false);
+ function fetchError(){
+ setErrors("Unable to fetch images");
+       setLoading(false);
+ }  
+ function fetch(){
+const url= 
+searchTerm === null ? "photos?" :`search/photos?query=${searchTerm}&`;
+Axios.get(
+      `${api}/${url}client_id=${secretkey}&page=${page}`
+    )
+          .then((response) => {
+            if(searchTerm===null){
+              fetchRandomPage(response)
+            }else{
+              fetchSearch(response);
+            }
        
-       
+         setLoading(false);
+      
+      })
+    .catch(e=>{
+      fetchError()
+    })
+  }
+     
+ function fetchSearch(response) {
+ if(page>1){
+                 setImage([...images,...response.data.results]);
+     
+        }else{
+           setImage([...response.data.results]);
+          
+        }
+   
+ }   
  
+ function fetchRandomPage(response){
+     setImage([...images,...response.data]);
+
+ }
+ 
+
  useEffect(() => {
          setLoading(true);
-    // if(searchTerm===null) return;
-    Axios.get( `${api}/photos?client_id=${secretkey}&page=${page}` )
- .then(res=>{
     
-             
-       
-              setImage([...images,...res.data]);
-              setLoading(false);
-       })
-      
- 
- .catch((e)=>{
-       setErrors("Unable to fetch images");
-       setLoading(false);
- });
+       fetch();
+  
 
  
     
-}, [page]);
- useEffect(() => {
-    if (searchTerm=== null) return;
-    Axios.get(
-      `${api}/search/photos?client_id=${secretkey}&page=${page}&query=${searchTerm}`
-    )
-      .then((response) => {
-        setImage([...response.data.results]);
-        setLoading(false);
-      })
-      .catch((e) => {
-        setErrors(["Unable to fetch images"]);
-        setLoading(false);
-      });
-  }, [searchTerm]);
+}, [page,searchTerm]);
+
  return [images,setImage,errors,loading];
 }
