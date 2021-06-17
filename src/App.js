fe8aeea0943d930769d810359b3aa42eec8e7436
Tanzim <tanzim67@gmail.com>
2021-06-17 17:28:50 +0600

    2-More dynamic values

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import './App.css';
 function App() {
   const name='Tanzim';
   const age=25;
+  const link='https://www.facebook.com/';
   return (
     <div className="App">
            <p>Hello World-{name} and age is {age}</p>
+           <p>{Math.random()*10}</p>
+           <p>{['Apple','Mango','Jackfruit']}</p>
+           <a href={link} target="_blank">Facebook</a>
     </div>
   );
 }
