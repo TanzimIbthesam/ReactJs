774d38c7fc2702ad22f6a6c5b675c211a80eea5e
Tanzim <tanzim67@gmail.com>
2021-06-17 17:43:53 +0600

    Component corrected

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 
 import './App.css';
-
+import Navbar from './Navbar'
 function App() {
+
   const name='Tanzim';
   const age=25;
   const link='https://www.facebook.com/';
   return (
     <div className="App">
+             <Navbar />
            <p>Hello World-{name} and age is {age}</p>
            <p>{Math.random()*10}</p>
            <p>{['Apple','Mango','Jackfruit']}</p>
-           <a href={link} target="_blank">Facebook</a>
+           <a href={link} >Facebook</a>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar=()=> {
+  
+    return (
+      <div className="App">
+            <a href="/">Hello</a>
+      </div>
+    );
+  }
+
+  export default Navbar;
