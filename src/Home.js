af7b45f5605ec22b6dd4216faf3a43806b12b226
Tanzim <tanzim67@gmail.com>
2021-06-18 21:07:20 +0600

    11-useEffect without dependencies

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@
  import { useState,useEffect } from 'react';
 import Blogs from './Blogs'
 const Home=()=>{
-
+const [name,setName]=useState("Tanzim");
 const [blogs,setBlogs ]=useState([
     {id:1,title:'Hello First Post',description:'Description of first post',author:'John'},
     {id:2,title:'Hello Second Post',description:'Description of second post',author:'John'},
@@ -16,11 +16,14 @@ const [blogs,setBlogs ]=useState([
  }
  useEffect(()=>{
      console.log("Use Effect");
- },[])
+     console.log(name);
+ },[name])
         return (
             <div className="">
                   
                    <Blogs blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+                   <button onClick={()=>setName('Tanzim Ibthesam')}>Change Name</button>
+                    The name is-{name}
             </div>
          
           );
