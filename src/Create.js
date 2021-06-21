53414dec96e8a8b62e02fe9773350b1585d53d01
Tanzim <tanzim67@gmail.com>
2021-06-21 22:17:13 +0600

    20-Submit form layout

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,41 @@
-const Create=()=>{
+import { useState } from "react";
 
-    return (
-        <div>
-            <h1>Create App</h1>
-        </div>
-    )
+const Create=()=>{
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [author, setAuthor] = useState('mario');
+     return (
+        <div className="create">
+        <h2>Add a New Blog</h2>
+        <form>
+          <label>Blog title:</label>
+          <input 
+            type="text" 
+            required 
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label>Blog body:</label>
+          <textarea
+            required
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+          <label>Blog author:</label>
+          <select
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          >
+            <option value="mario">mario</option>
+            <option value="yoshi">yoshi</option>
+          </select>
+          <button>Add Blog</button>
+        </form>
+      </div>
+
+     )
+     
+    
 
 }
 
