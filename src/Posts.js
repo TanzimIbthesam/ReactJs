31d50174b1dec5a417be82a80597a8cdfc9d0c63
Tanzim <tanzim67@gmail.com>
2021-07-04 14:22:50 +0600

    8-Fetching data with useEffect-2

--- src/Posts.js
+++ src/Posts.js
@@ -4,26 +4,31 @@ import { useEffect, useState } from "react";
 
 
 const Posts=()=>{
-    const [posts,SetPosts]=useState([]);
+    // const [posts,SetPosts]=useState([]);
+    //fetching single post
+    const [post,SetPost]=useState({});
+    const [id,setId]=useState(1)
 
     useEffect(()=>{
-        axios.get('https://jsonplaceholder.typicode.com/todos')
-        .then(res=>SetPosts(res.data))
+        axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res=>SetPost(res.data))
         .catch(err=>console.log(err))
 
-    },[])
+    },[post])
    
     
     
 
     return (
         <div>
-            {posts.map(post=>(
+            <input type="text" value={id} onChange={(e)=>setId(e.target.value)} />
+            {post.title}
+            {/* {posts.map(post=>(
                 <div> 
                     {post.title}
                     </div>
 
-            ))}
+            ))} */}
         </div>
     )
 
