ae771ddb8ab8a77572c7bf808c5227afab18a378
Tanzim <tanzim67@gmail.com>
2021-07-19 21:13:42 +0600

    5-If else React

--- reactcourse/src/App.js
+++ reactcourse/src/App.js
@@ -1,41 +1,21 @@
 
 
- import { useState } from 'react';
+
 import './App.css';
 //  import ClassUser from './ClassUser';
 // import FunctionalUser from './FunctionalUser';
 
-
+import IfElse from './IfElse'
 function App() {
     // const [name,setName]=useState("Tanzim");
-    const [text,setText]=useState("");
-    const [options,setOptions]=useState("");
-    const [check,setCheck]=useState("")
- const addProfile=(e)=>{
- e.preventDefault()
- console.log(text,options,check);
- }
+    
+  
+ 
   return (
-    <div className="App">
-       <form onSubmit={addProfile}>
-             <input type="text" onChange={(e)=>setText(e.target.value)}></input><br></br>
-             {/* <select onChange={(e)=>setOptions(e.target.value)}>
-               <options>Web designer</options>
-               <options>Web developer</options>
-               
-             </select><br></br> */}
-             <select onChange={(e)=>setOptions(e.target.value)}>
-          <option >Volvo</option>
-           <option >Saab</option>
-       <option >Mercedes</option>
-        <option value="audi">Audi</option>
-</select>
-            <input type="checkbox" onChange={(e)=>setCheck(e.target.value)} ></input><br></br>
-            <p>Are you sure you agree with terms and conditions?</p>
-            
-            <button>SUBMIT</button>
-       </form>
-    </div>
+    <div>
+     <IfElse />
+ </div>
+
   );
 }
 
