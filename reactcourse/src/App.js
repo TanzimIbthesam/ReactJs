9d5837f29183bd30663d19386910bea6e60c04ba
Tanzim <tanzim67@gmail.com>
2021-07-19 18:01:18 +0600

    2-Input Value

--- reactcourse/src/App.js
+++ reactcourse/src/App.js
@@ -1,21 +1,44 @@
 
 
-// import { useState } from 'react';
+ import { useState } from 'react';
 import './App.css';
- import ClassUser from './ClassUser';
+//  import ClassUser from './ClassUser';
 // import FunctionalUser from './FunctionalUser';
 
 
 function App() {
-  //  const [name,setName]=useState("Tanzim");
-  function updateName(){
-    //  setName("Tanzim Ibthesam")
-     console.log("Clicked");
+    const [name,setName]=useState("Tanzim");
+    const [print,setPrint]=useState(false)
+  // function updateName(){
+  //   //  setName("Tanzim Ibthesam")
+  //    console.log("Clicked");
 
+  //  }
+
+  function getData(e){
+     e.preventDefault();
+      setName(e.target.value)
+  }
+   function setPrintValue(e){
+    e.preventDefault();
+    setPrint(true)
    }
+
+ 
+  
   return (
     <div className="App">
-       <ClassUser fullName="Tanzim Ibthesam" updateName={updateName} /> 
+       {print ?
+        <h1>{name}</h1>
+       :null}
+     
+       <form>
+        
+         
+         <input type="text" onChange={(e)=>getData(e)} ></input>
+         <button onClick={(e)=>setPrintValue(e)}>Print</button>
+       </form>
+       {/* <ClassUser fullName="Tanzim Ibthesam" updateName={updateName} />  */}
       {/* <FunctionalUser name="Tanzim" updateName={updateName} /> */}
       {/* <h1>{name}</h1>
      <button onClick={updateName}>Change Name</button> */}
