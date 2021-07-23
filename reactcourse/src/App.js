972371034f4b6aeb89833d37b3766450c64bc089
Tanzim <tanzim67@gmail.com>
2021-07-23 16:44:07 +0600

    17-UseMemo

--- reactcourse/src/App.js
+++ reactcourse/src/App.js
@@ -1,33 +1,37 @@
-import React, {  PureComponent } from 'react';
+import React, { useMemo, useState } from 'react';
 import './App.css';
 // import PropsUseEffect from './PropsUseEffect';
  
 import 'bootstrap/dist/css/bootstrap.min.css'
- import User from './User';
+
+//  import User from './User';
 
 // import ClassUserTwo from './ClassUsertwo';
 // import ClassUserTwo from './ClassUsertwo';
 // import Test from './Test';
 
-class App extends PureComponent{
-  //Pure Compoent is used for it to stop rerendering
-  constructor(){
-    super()
-    this.state={
-      count:0
-    }
-  }
-   render(){
-     console.log("Rerender");
-     return(
-       <>
-          <User count={this.state.count} />
-          <button onClick={()=>this.setState({count:1})}>Click</button> 
-       </>
-     )
-   }
+const App=()=>{
+  const [count,setCount]=useState(10);
+  const [updateCount,setUpdateCount]=useState(10);
+  // function newcount(){
+   
+    
+  // }
+    const countMemo=useMemo(function newcount(){
+      console.log("Count updated");
+      return count+1
+    },[count])
 
-
+   return(
+     <>
+       <button onClick={()=>setCount(count+1)}> Count</button>
+       <p> Count-{count}</p>
+       <button onClick={()=>setUpdateCount(updateCount+1)}> Count</button>
+       <p>Update Count-{updateCount}</p>
+       {/* { newcount() } */}
+       {countMemo}
+     </>
+   )
 }
 
 
